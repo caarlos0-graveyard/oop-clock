f75f58140f3fbc66b438c9bdd0fd7d1dafe29662
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-02-03 13:51:16 -0200

    qulice fixes

--- src/test/java/com/carlosbecker/clock/ClockTest.java
+++ src/test/java/com/carlosbecker/clock/ClockTest.java
@@ -32,9 +32,14 @@ import org.junit.Test;
  * @author Carlos Alexandro Becker (caarlos0@gmail.com)
  * @version $Id$
  * @since 0.1
- * @checkstyle MagicNumberCheck (100 lines)
+ * @checkstyle MagicNumberCheck (200 lines)
+ * @checkstyle MethodNameCheck (200 lines)
  */
+@SuppressWarnings("PMD.TooManyMethods")
 public final class ClockTest {
+    /**
+     * Clock should throw an exception if hour is not between 0 and 11.
+     */
     @Test
     public void throwsExceptionWithIncorrectHour() {
         final String message = "Hour must be between";
@@ -46,6 +51,9 @@ public final class ClockTest {
             .hasMessageContaining(message);
     }
 
+    /**
+     * Clock should throw an exception if minute is not between 0 and 59.
+     */
     @Test
     public void throwsExceptionWithIncorrectMinute() {
         final String message = "Minute must be between";
@@ -57,51 +65,84 @@ public final class ClockTest {
             .hasMessageContaining(message);
     }
 
+    /**
+     * Clock should calculate the angle for 0:00.
+     */
     @Test
     public void calculatesMidnight() {
         this.assertAngle(0, 0).isEqualTo(0);
     }
 
+    /**
+     * Clock should calculate the angle for 3:00.
+     */
     @Test
     public void calculates3OClock() {
         this.assertAngle(3, 0).isEqualTo(90);
     }
 
+    /**
+     * Clock should calculate the angle for 6:00.
+     */
     @Test
     public void calculates6OClock() {
         this.assertAngle(6, 0).isEqualTo(180);
     }
 
+    /**
+     * Clock should calculate the angle for 9:00.
+     */
     @Test
     public void calculates9OClock() {
         this.assertAngle(9, 0).isEqualTo(90);
     }
 
+    /**
+     * Clock should calculate the angle for 2:20.
+     */
     @Test
     public void calculates220() {
         this.assertAngle(2, 20).isEqualTo(50);
     }
 
+    /**
+     * Clock should calculate the angle for 5:24.
+     */
     @Test
     public void calculates524() {
         this.assertAngle(5, 24).isEqualTo(18);
     }
 
+    /**
+     * Clock should calculate the angle for 9:45.
+     */
     @Test
     public void calculates945() {
         this.assertAngle(9, 45).isEqualTo(23);
     }
 
+    /**
+     * Clock should calculate the angle for 11:20.
+     */
     @Test
     public void calculates1120() {
         this.assertAngle(11, 20).isEqualTo(140);
     }
 
+    /**
+     * Clock should calculate the angle for 4:20.
+     */
     @Test
     public void calculates420() {
         this.assertAngle(4, 20).isEqualTo(10);
     }
 
+    /**
+     * Helper method to assert an angle for a given hour and minute.
+     * @param hour Hour.
+     * @param minute Minute.
+     * @return Assertion.
+     */
     private AbstractIntegerAssert<?> assertAngle(
         final int hour, final int minute
     ) {
